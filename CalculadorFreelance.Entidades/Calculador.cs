b3deb75bd4553d98959657d93b06c931d08b6f45
Candela Fernandez <cande.fdz12@gmail.com>
2024-05-11 08:58:18 -0300

    subida

---
+++ CalculadorFreelance.Entidades/Calculador.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculadorFreelance.Entidades
+{
+    public class Calculador
+    {
+        public string nombre { get; set; }
+        public int horas { get; set; }
+        public int valorHora { get; set; }
+        public int montoTotal { get; set; }
+
+    }
+}
---
+++ CalculadorFreelance.Servicios/ServiceCalculador.cs
@@ -0,0 +1,28 @@
+﻿using CalculadorFreelance.Entidades;
+
+namespace CalculadorFreelance.Servicios
+{
+    public interface IServiceCalculador
+    {
+        public void Calcular(Calculador calculador);
+        public List<Calculador> ObtenerResultados();
+    }
+    public class ServiceCalculador : IServiceCalculador
+    {
+        private static List<Calculador> lista = new List<Calculador>();
+        public ServiceCalculador()
+        {
+            
+        }
+        public void Calcular(Calculador calculador)
+        {
+            calculador.montoTotal = calculador.horas * calculador.valorHora;
+            lista.Add(calculador);
+        }
+
+        public List<Calculador> ObtenerResultados()
+        {
+            return lista.ToList();
+        }
+    }
+}
